21c40bbb7af59ce9d81f44efebe5b5e0b8cd18df
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-13 09:36:27 +0300

    all added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int n = x;
+            int s = 0;
+            while (n != 0) {
+                s += n % 10;
+                n /= 10;
+            }
+            Console.Write("{0} -> {1}\n\n", x, s);
+            Console.ReadKey();
+        }
+    }
+}
